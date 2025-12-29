d5d3df02bdd7e89ebf5a26f7ce8ce08f5208c89d
Supawat Tanmanee <supawat.tanmanee@elysiannxt.com>
2025-12-29 11:32:13 +0700

    docs: add editorconfig to specified rule

--- src/KubeTools4Dev.Core/Services/UpdateService.cs
+++ src/KubeTools4Dev.Core/Services/UpdateService.cs
@@ -13,19 +13,7 @@ public class UpdateService : IUpdateService
     /// <summary>
     /// The update manager
     /// </summary>
-    private readonly UpdateManager _updateManager;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="UpdateService"/> class.
-    /// </summary>
-    public UpdateService()
-    {
-        // We can initialize this later or rely on DI to pass the manager
-        // For now, simpler to just use the static logic or wrap it.
-        // But purely static usage of VelopackApp doesn't fit well with this service if we want to mock it.
-        // Velopack's UpdateManager is the way to go.
-        _updateManager = new UpdateManager(new GithubSource("https://github.com/DevilDogTG/KubeTools4Dev", null, false));
-    }
+    private readonly UpdateManager _updateManager = new(new GithubSource("https://github.com/DevilDogTG/KubeTools4Dev", null, false));
 
     /// <summary>
     /// Gets the current version.
@@ -33,13 +21,7 @@ public class UpdateService : IUpdateService
     /// <value>
     /// The current version.
     /// </value>
-    public string CurrentVersion
-    {
-        get
-        {
-            return _updateManager?.CurrentVersion?.ToString() ?? "0.0.0";
-        }
-    }
+    public string CurrentVersion => _updateManager?.CurrentVersion?.ToString() ?? "0.0.0";
 
     /// <summary>
     /// Checks for updates asynchronous.
@@ -47,7 +29,11 @@ public class UpdateService : IUpdateService
     /// <returns></returns>
     public async Task<UpdateInfo?> CheckForUpdatesAsync()
     {
-        if (_updateManager == null) return null;
+        if (_updateManager == null)
+        {
+            return null;
+        }
+
         try
         {
             return await _updateManager.CheckForUpdatesAsync();
@@ -65,7 +51,11 @@ public class UpdateService : IUpdateService
     /// <param name="updateInfo">The update information.</param>
     public async Task DownloadAndUpdateAsync(UpdateInfo updateInfo)
     {
-        if (_updateManager == null) return;
+        if (_updateManager == null)
+        {
+            return;
+        }
+
         await _updateManager.DownloadUpdatesAsync(updateInfo);
         _updateManager.ApplyUpdatesAndRestart(updateInfo);
     }
